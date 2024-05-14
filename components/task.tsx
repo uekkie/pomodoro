fd5ddd7009d9fde78ae0f0f1d8520f48fb5f9672
Hiroshi Ueki <ueki@sonicgarden.jp>
2024-05-14 22:54:01 +0900

    タイマー開始と経過時間表示

--- components/task.tsx
+++ components/task.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { Task as TaskType } from '@/types/task'
 import { Pause, Play, Trash } from 'lucide-react'
+import { useStopwatch } from 'react-timer-hook'
 import useLocalStorageState from 'use-local-storage-state'
 
 export default function Task({ task }: { task: TaskType }) {
@@ -10,19 +11,27 @@ export default function Task({ task }: { task: TaskType }) {
     defaultValue: [],
   })
 
+  const { totalSeconds, seconds, minutes, hours, isRunning, start, pause } =
+    useStopwatch()
+
+  const getFormattedTime = (num: number): string => {
+    return num.toString().padStart(2, '0')
+  }
+
   return (
     <div key={task.id} className="p-4 flex items-center border rounded-md">
       {task.title}
 
       <span className="flex-1"></span>
+      <p className="text-muted-foreground tabular-nums text-sm">
+        {getFormattedTime(minutes)}:{getFormattedTime(seconds)}
+      </p>
 
       <Button
         variant="ghost"
         className="text-muted-foreground"
         size="icon"
-        onClick={() => {
-          setTasks((prev) => prev.filter((item) => item.id !== task.id))
-        }}
+        onClick={start}
       >
         <Play size={18} />
         <span className="sr-only">タスク開始</span>
@@ -32,9 +41,7 @@ export default function Task({ task }: { task: TaskType }) {
         variant="ghost"
         className="text-muted-foreground"
         size="icon"
-        onClick={() => {
-          setTasks((prev) => prev.filter((item) => item.id !== task.id))
-        }}
+        onClick={pause}
       >
         <Pause size={18} />
         <span className="sr-only">タスク停止</span>
